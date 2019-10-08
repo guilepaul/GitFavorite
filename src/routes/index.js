f598916fb5133f5f1a090d4b1905fd88d9139b72
Guilherme Di Paula <guilhermedipaula@Guilhermes-MBP.mshome.net>
2019-10-08 18:40:27 -0300

    implementaçao redux saga

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from '../pages/Main';
+import Footer from '../components/Footer';
+
+const Routes = () => (
+  <BrowserRouter>
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+
+      <Footer />
+    </>
+  </BrowserRouter>
+);
+
+export default Routes;
